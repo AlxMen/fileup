21da182a61777df6ff11c96e60b961d8df49ceed
AlxMen <alejandrocs402@gmail.com>
2024-06-28 13:02:24 +0100

    implementando funcionalidad para subir un archivo aun servidor local

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,42 @@
+"use client";
+
+import { useState } from "react";
 
 export default function HomePage() {
+  const [file, setFile] = useState({});
+  
+
   return (
-    <div>HomePage</div>
-  )
+    <>
+      <form onSubmit={async (e) => {
+        e.preventDefault()
+        if (!file) return
+        
+        const form = new FormData()
+        form.set('file', JSON.stringify(file))
+
+        const res = await fetch('/upload', {
+          method: "POST",
+          body: form
+        })
+        const data = res.json()
+        console.log(data);
+        
+      }}>
+        <label htmlFor="file">Upload File:</label>
+        <input
+          type="file"
+          name="file"
+          id="file"
+          onChange={(e) => {
+            if (!e.target.files) return
+            setFile(e.target.files[0])
+          }}
+        />
+        <button className="bg-slate-400 border border-black p-1">
+          Upload
+        </button>
+      </form>
+    </>
+  );
 }
--- src/app/upload/route.ts
+++ src/app/upload/route.ts
@@ -1,3 +1,10 @@
-export async function GET(request: Request) {
-  return new Response("uploading file!")
+import { arrayBuffer } from "stream/consumers"
+
+
+export async function POST(request: Request) {
+  
+  const data = await request.formData()
+  const file = data.get('file')  
+
+  return new Response(JSON.stringify({message:"uploading File"}))
 }
