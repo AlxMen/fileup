f430d0f3d38b72b6a3247c90af32845ff81dd7a0
AlxMen <alejandrocs402@gmail.com>
2024-07-01 12:42:53 +0100

    comprobaciones de codigo por errores de compatibilidad entre versiones

--- src/app/upload/route.ts
+++ src/app/upload/route.ts
@@ -1,10 +1,52 @@
-import { arrayBuffer } from "stream/consumers"
+import { NextApiRequest, NextApiResponse } from "next";
+import formidable from "formidable";
+import fs from "fs";
+import path from "path";
 
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
-export async function POST(request: Request) {
-  
-  const data = await request.formData()
-  const file = data.get('file')  
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const form = new formidable.IncomingForm();
+    form.parse(req, async (err, fields, files) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: "Error al procesar el archivo" });
+      }
 
-  return new Response(JSON.stringify({message:"uploading File"}))
+      const file = files.file as unknown as formidable.File;
+      if (!file) {
+        return res
+          .status(400)
+          .json({ error: "No se ha seleccionado ningún archivo" });
+      }
+
+      const uploadDir = path.join(process.cwd(), "public", "uploads");
+      if (!fs.existsSync(uploadDir)) {
+        fs.mkdirSync(uploadDir, { recursive: true });
+      }
+
+      const newFilePath = file.originalFilename
+        ? path.join(uploadDir, file.originalFilename)
+        : path.join(uploadDir, "defaultFileName.txt");
+      await fs.promises.rename(file.filepath, newFilePath);
+
+      return res
+        .status(200)
+        .json({
+          message: "Archivo subido correctamente",
+          url: `/uploads/${file.originalFilename}`,
+        });
+    });
+  } else {
+    res.setHeader("Allow", "POST");
+    return res.status(405).end("Método no permitido");
+  }
 }
